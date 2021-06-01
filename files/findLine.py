174326ae9125238860f95028d9583c53040ddcc3
malay1803 <18acharyamalay@gmail.com>
2021-06-02 00:35:20 +0530

    added try and except (ex2)

--- files/findLine.py
+++ files/findLine.py
@@ -5,7 +5,11 @@
 # When you encounter a line that starts with "X-DSPAM-Confidence:" pull apart the line to extract the floating-point number on the line. Count these lines and then compute the total of the spam confidence values from these lines. When you reach the end of the file, print out the average spam confidence.
 
 fName = input("Enter your file name : ")
-fHand = open(fName)
+try:
+    fHand = open(fName)
+except:
+    print("Invalid file name")
+    quit()
 sum = 0
 count=0
 for line in fHand :
